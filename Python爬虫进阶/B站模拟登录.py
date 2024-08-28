d1e2d86d020f0a99cc6ec8870961949604c0ac34
CODE_OF_ZHANG <3363967646@qq.com>
2024-08-28 22:03:29 +0800

    B站模拟登录

--- Python爬虫进阶/B站模拟登录.py
+++ Python爬虫进阶/B站模拟登录.py
@@ -1,13 +1,16 @@
 import time
 
+from PIL import Image
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from chaojiying import Chaojiying_Client
+# pip install pillow -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 
-class BiliBili:
+class BiliBili(object):
     def __init__(self, username, password):
         self.username = username
         self.password = password
@@ -41,18 +44,37 @@ class BiliBili:
         self.driver.save_screenshot('back_img.png')
         print('验证窗口加载完成')
         div_img = self.driver.find_element(By.CLASS_NAME, 'geetest_panel_next')
+        print(div_img.location)
         img_location = div_img.location
+        x1, y1 = img_location['x'], img_location['y']
         img_size = div_img.size
-        print(img_location, img_size)
-        x1, y1 = int(img_location['x']), int(img_location['y'])
+        print(img_size)
         x2, y2 = x1 + img_size['width'], y1 + img_size['height']
+        back_img = Image.open('back_img.png')
+        img = back_img.crop((x1, y1, x2, y2))
+        img.save('yzm.png')
+        return div_img
+
+    def clock_font(self, dic, yzm_img):
+        for x, y in dic.items():
+            action = ActionChains(self.driver)
+            action.move_to_element_with_offset(yzm_img, int(x), int(y)).click().perform()
+        self.driver.find_element(By.CLASS_NAME, 'geetest_commit_tip').click()
         pass
 
     def run(self):
         self.login()
         self.login_image()
-        self.save_image()
-        # 在这里可以添加
+        yzm_img = self.save_image()
+        chaojiying = Chaojiying_Client('15576806087', 's15576806087', '962830')
+        im = open('yzm.png', 'rb').read()
+        yzm = chaojiying.PostPic(im, 9004)['pic_str']
+        lic_list = yzm.split('|')
+        dic = {}
+        for i in lic_list:
+            s = i.split(',')
+            dic[s[0]] = s[1]
+        self.clock_font(dic, yzm_img)
 
 
 if __name__ == '__main__':
